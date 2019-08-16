ec309f194e5152385daf51d2e94454614e791ade
Shachar Itzhaky <corwin.amber@gmail.com>
2019-08-16 13:59:00 +0300

    Simplified example.

--- example/example.js
+++ example/example.js
@@ -3,23 +3,27 @@ const {DocumentSlot} = require('automerge-slots');
 const {AutomergeCodeMirror} = require('..');
 
 
-var docset = new Automerge.DocSet();
 
-docset.setDoc('doc', Automerge.change(Automerge.init(),
-    doc => doc.text = new Automerge.Text()));
+var docset = new Automerge.DocSet(),
+    slot = new DocumentSlot(docset, 'doc').path(['text']);
 
-var slot = new DocumentSlot(docset, 'doc').path(['text']);
+slot.set(new Automerge.Text());  /* creates a document with an empty Text */
 
+
+/**
+ * Creates a new CodeMirror and links it to the slot.
+ * All instances created thus will be synchronized with the same Text.
+ */
 function createSheet() {
     var div = document.createElement('div');
     div.classList.add('sheet');
     document.body.appendChild(div);
-    var editor = new CodeMirror(div, {
-        viewportMargin: Infinity
-    });
-    new AutomergeCodeMirror(editor, slot, {debounce: {wait: 0}});
+
+    var editor = new CodeMirror(div, {viewportMargin: Infinity});
+
+    return new AutomergeCodeMirror(editor, slot, {debounce: {wait: 0}});
 }
 
 
 
-Object.assign(window, {Automerge, DocumentSlot, AutomergeCodeMirror, createSheet});
+Object.assign(window, {docset, slot, createSheet});
